a7867a20a1ab9e111655d28f8e61bb4d776293ef
BastianB78 <54079294+BastianB78@users.noreply.github.com>
2022-11-26 00:34:13 -0300

    Added base front testing

---
+++ front/client/index.js
@@ -0,0 +1,16 @@
+require('dotenv').config();
+const express = require('express');
+const app = express();
+const http = require('http');
+const server = http.createServer(app);
+const serverport = process.env.SERVER_PORT;
+const serveraddress = process.env.SERVER_ADDRESS;
+
+app.use(express.static(__dirname + '/node_modules'));
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
+  });
+
+  server.listen(process.env.PORT, () => {
+    console.log(`listening on ${serveraddress}:${process.env.PORT}`);
+  });
